742953f96b576734245c2b4609fb97b22f189563
Rami-Ismael <rami.m.ismael@gmail.com>
2024-01-04 05:31:31 -0600

    feed the new parramter to the env_creator

--- pufferlib/environments/pokemon_red/environment.py
+++ pufferlib/environments/pokemon_red/environment.py
@@ -11,6 +11,6 @@ def make_env(headless: bool = True, state_path=None ,
              reward_the_agent_for_completing_the_pokedex = False):
     '''Pokemon Red'''
     env = env_creator(headless=headless, state_path=state_path , 
-                      reward_the_agent_for_the_number_of_pokemon_caught = reward_the_agent_for_completing_the_pokedex)
+                      reward_the_agent_for_completing_the_pokedex=  reward_the_agent_for_completing_the_pokedex)
     return pufferlib.emulation.GymnasiumPufferEnv(env=env,
         postprocessor_cls=pufferlib.emulation.BasicPostprocessor)
