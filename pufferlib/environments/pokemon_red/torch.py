cdeaad768285b6f878a3549680b7e3078f751d31
Rami-Ismael <rami.m.ismael@gmail.com>
2024-01-12 23:18:37 -0600

    Update torch.py with additional input and hidden size

--- pufferlib/environments/pokemon_red/torch.py
+++ pufferlib/environments/pokemon_red/torch.py
@@ -1,8 +1,8 @@
 import pufferlib.models
 
 class Recurrent:
-    input_size:int = 512
-    hidden_size:int = 512
+    input_size:int = 512 + 32
+    hidden_size:int = 512 + 32
     num_layers:int = 1
 
 import torch
@@ -20,7 +20,7 @@ class Policy(pufferlib.models.Policy):
     def __init__( self, env, 
                  frame_stack = 3, 
                  hiden_size = 512, 
-                 output_size = 512,
+                 output_size = 512 +32,
                  flat_size = 64*5*6,
                  downsample = 1
                  ):
@@ -40,7 +40,7 @@ class Policy(pufferlib.models.Policy):
         self.channels_last = True
         self.downsample = downsample
         
-        self.network = nn.Sequential(
+        self.nature_cnn = nn.Sequential(
             pufferlib.pytorch.layer_init(nn.Conv2d(frame_stack, 32, 8, stride=4)),
             nn.ReLU(),
             pufferlib.pytorch.layer_init(nn.Conv2d(32, 64, 4, stride=2)),
@@ -49,8 +49,9 @@ class Policy(pufferlib.models.Policy):
             nn.ReLU(),
             nn.Flatten(),
             pufferlib.pytorch.layer_init(nn.Linear(flat_size, hiden_size)),
-            nn.ReLU(),
         )
+        self.player_row_embedding = nn.Embedding(444,16)
+        self.player_column_embedding = nn.Embedding(436,16)
         self.actor = pufferlib.pytorch.layer_init(nn.Linear(output_size, self.num_actions), std=0.01)
         self.value_fn = pufferlib.pytorch.layer_init(nn.Linear(output_size, 1), std=1)
     def encode_observations(self, env_outputs):
@@ -61,7 +62,15 @@ class Policy(pufferlib.models.Policy):
             #print("observations: ", observations.shape)
         if self.downsample > 1:
             observations = observations[:, :, ::self.downsample, ::self.downsample]
-        return self.network(observations.float() / 255.0), None
+        #return self.network(observations.float() / 255.0), None
+        img = self.nature_cnn(observations.float() / 255.0)
+        #print("img: ", img.shape)
+        player_row = self.player_row_embedding(env_outputs["player_row"]).squeeze(1).to(torch.int)
+        #print("player_row: ", player_row.shape)
+        player_col = self.player_column_embedding(env_outputs["player_column"]).squeeze(1).to(torch.int)
+        #print("player_col: ", player_col.shape)
+        return F.relu(torch.cat((img, player_row, player_col), dim=1)), None
+        
 
     def decode_actions(self, flat_hidden, lookup, concat=None):
         action = self.actor(flat_hidden)
