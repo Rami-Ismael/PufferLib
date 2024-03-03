3f4c9c25131517e9cfbcc1f2ebb9a5b5d3d843c9
Rami-Ismael <rami.m.ismael@gmail.com>
2024-03-03 16:24:15 -0600

    Update done_training function to use stricter thresholds

--- clean_pufferl.py
+++ clean_pufferl.py
@@ -606,11 +606,11 @@ def rollout(env_creator, env_kwargs, agent_creator, agent_kwargs,
 
 def done_training(data):
     try:
-        if data.losses.explained_variance < -.3:
-            print("Explain variance is smaller than -.3")
+        if data.losses.explained_variance < -.8:
+            print("Explain variance is smaller than -.4")
             return True
-        if data.losses.policy_loss > .3:
-            print("Policy loss is bigger than .3")
+        if data.losses.policy_loss > .8:
+            print("Policy loss is bigger than .4")
             return True , "Policy loss is high"
     except Exception as e:
         with open("error.txt", "a") as f:
