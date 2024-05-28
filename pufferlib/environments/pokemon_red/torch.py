dbec21a79c9561f0a20831bada4116c184901439
Rami-Ismael <rami.m.ismael@gmail.com>
2024-05-28 06:46:09 -0500

    Getting ready for to move all the nn to policy.nn

--- pufferlib/environments/pokemon_red/torch.py
+++ pufferlib/environments/pokemon_red/torch.py
@@ -1,8 +1,11 @@
+import pdb
 from functools import partial
 import torch
-
+import torch
+import torch.nn as nn
 import pufferlib.models
-
+import pufferlib.models
+import pufferlib.pytorch
 
 class Recurrent(pufferlib.models.LSTMWrapper):
     def __init__(self, env, policy,
@@ -10,27 +13,54 @@ class Recurrent(pufferlib.models.LSTMWrapper):
         super().__init__(env, policy,
             input_size, hidden_size, num_layers)
 
-class Policy(pufferlib.models.Convolutional):
-    def __init__(self, env,
+class Policy(nn.Module):
+    def __init__(self, env, *args, framestack, flat_size,
             input_size=512, hidden_size=512, output_size=512,
-            framestack=4, flat_size=64*5*6):
-        super().__init__(
-            env=env,
-            input_size=input_size,
-            hidden_size=hidden_size,
-            output_size=output_size,
-            framestack=framestack,
-            flat_size=flat_size,
-            channels_last=True,
+            channels_last=False, downsample=1, **kwargs):
+        '''The CleanRL default NatureCNN policy used for Atari.
+        It's just a stack of three convolutions followed by a linear layer
+        
+        Takes framestack as a mandatory keyword argument. Suggested default is 1 frame
+        with LSTM or 4 frames without.'''
+        super().__init__()
+        self.channels_last = True
+        self.downsample = downsample
+        self.emulated = env.emulated
+        print(f"The emulated environment is {self.emulated}")
+        self.dtype = pufferlib.pytorch.nativize_dtype(self.emulated)
+        print(f"The dtype is {self.dtype}")
+        self.network= nn.Sequential(
+            pufferlib.pytorch.layer_init(nn.Conv2d(3, 32, 8, stride=4)),
+            nn.ReLU(),
+            pufferlib.pytorch.layer_init(nn.Conv2d(32, 64, 4, stride=2)),
+            nn.ReLU(),
+            pufferlib.pytorch.layer_init(nn.Conv2d(64, 64, 3, stride=1)),
+            nn.ReLU(),
+            nn.Flatten(),
+            pufferlib.pytorch.layer_init(nn.Linear(1920, hidden_size)),
+            nn.ReLU(),
         )
+        self.actor = pufferlib.pytorch.layer_init(
+            nn.Linear(hidden_size, env.single_action_space.n), std=0.01)
+        self.value_fn = pufferlib.pytorch.layer_init(
+            nn.Linear(output_size, 1), std=1)
 
+    def forward(self, observations):
+        hidden, lookup = self.encode_observations(observations)
+        actions, value = self.decode_actions(hidden, lookup)
+        return actions, value
 
-'''
-class Policy(pufferlib.models.ProcgenResnet):
-    def __init__(self, env, cnn_width=16, mlp_width=512):
-        super().__init__(
-            env=env,
-            cnn_width=cnn_width,
-            mlp_width=mlp_width,
-        )
-'''
+    def encode_observations(self, observations):
+        env_outputs = pufferlib.pytorch.nativize_tensor(observations, self.dtype)
+        #pdb.set_trace()
+        if self.channels_last:
+            #observations = env_outputs["screen"].permute(0, 3, 1, 2)
+            observations = env_outputs["screen"].permute(0, 3, 1, 2)
+        if self.downsample > 1:
+            observations = observations[:, :, ::self.downsample, ::self.downsample]
+        return self.network(observations.float() / 255.0), None
+
+    def decode_actions(self, flat_hidden, lookup, concat=None):
+        action = self.actor(flat_hidden)
+        value = self.value_fn(flat_hidden)
+        return action, value
