b7a849beda2f509baa40f40a34dcac23370e5f0a
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-11 00:12:39 -0500

    Add clipping to the reward function

--- pufferlib/environments/pokemon_red/environment.py
+++ pufferlib/environments/pokemon_red/environment.py
@@ -1,9 +1,12 @@
+import math
 from pdb import set_trace as T
 
 import gymnasium
 import functools
 
 from pokegym import Environment
+
+from gymnasium.wrappers.transform_reward import  TransformReward
 
 import pufferlib.emulation
 import pufferlib.postprocess
@@ -17,6 +20,7 @@ def make(name, headless: bool = True, state_path=None):
     env = Environment(headless=headless, state_path=state_path)
     env = RenderWrapper(env)
     #env = pufferlib.postprocess.EpisodeStats(env) we are removing this because I want to see the performances for each episode
+    env = TransformReward(env, lambda x:  max(min(1.0 , x) , -1.0)) # clip the reward to -1 to 1 
     return pufferlib.emulation.GymnasiumPufferEnv(env=env)
 
 class RenderWrapper(gymnasium.Wrapper):
