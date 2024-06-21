755a78b1dae0eac0068c4e08a0db0245f4e5fdc1
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-21 18:09:38 -0500

    chore: Update Pokemon Red environment with new reward coefficients and display info interval divisor

--- pufferlib/environments/pokemon_red/environment.py
+++ pufferlib/environments/pokemon_red/environment.py
@@ -17,10 +17,20 @@ from rich import print as print
 def env_creator(name='pokemon_red'):
     return functools.partial(make, name)
 
-def make(name, headless: bool = True, state_path=None , display_info_interval_divisor = 2048 , EpisodeStats = False , max_episode_steps = 65536 , max_reward_clip = 1.0):
+def make(name, headless: bool = True, state_path=None , 
+         display_info_interval_divisor = 2048 , 
+         EpisodeStats = False , max_episode_steps = 65536 , 
+         max_reward_clip = 1.0, 
+        reward_for_increase_pokemon_level_coef =  1.1 , 
+        reward_for_explore_unique_coor_coef = .4
+         ):
     #print(f"The current diosplayin of interval divisor is {display_info_interval_divisor}")
     '''Pokemon Red'''
-    env = Environment(headless=headless, state_path=state_path , display_info_interval_divisor=display_info_interval_divisor , max_episode_steps=max_episode_steps)
+    env = Environment(headless=headless, 
+                      state_path=state_path , display_info_interval_divisor=display_info_interval_divisor , 
+                      max_episode_steps=max_episode_steps , 
+                      reward_for_explore_unique_coor_coef=reward_for_explore_unique_coor_coef ,
+                        reward_for_increase_pokemon_level_coef=reward_for_increase_pokemon_level_coef)
     env = RenderWrapper(env)
     if EpisodeStats:
         env = pufferlib.postprocess.EpisodeStats(env) #we are removing this because I want to see the performances for each episode
