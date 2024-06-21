655662485040752d5ccd631f09c6ec51074d8c72
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-20 23:30:26 -0500

    chore: Clip reward values within specified range in Pokemon Red environment

--- pufferlib/environments/pokemon_red/environment.py
+++ pufferlib/environments/pokemon_red/environment.py
@@ -17,14 +17,14 @@ from rich import print as print
 def env_creator(name='pokemon_red'):
     return functools.partial(make, name)
 
-def make(name, headless: bool = True, state_path=None , display_info_interval_divisor = 2048 , EpisodeStats = False , max_episode_steps = 65536):
+def make(name, headless: bool = True, state_path=None , display_info_interval_divisor = 2048 , EpisodeStats = False , max_episode_steps = 65536 , max_reward_clip = 1.0):
     #print(f"The current diosplayin of interval divisor is {display_info_interval_divisor}")
     '''Pokemon Red'''
     env = Environment(headless=headless, state_path=state_path , display_info_interval_divisor=display_info_interval_divisor , max_episode_steps=max_episode_steps)
     env = RenderWrapper(env)
     if EpisodeStats:
         env = pufferlib.postprocess.EpisodeStats(env) #we are removing this because I want to see the performances for each episode
-    env = TransformReward(env, lambda x:  max(min(1.0 , x) , -1.0)) # clip the reward to -1 to 1 
+    env = TransformReward(env, lambda x:  max(min(max_reward_clip , x) , -1 * max_reward_clip)) # clip the reward to -1 to 1 
     return pufferlib.emulation.GymnasiumPufferEnv(env=env)
 
 class RenderWrapper(gymnasium.Wrapper):
