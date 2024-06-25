15021efbee5f73b0613a397ca8a8fd109efe8125
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-24 23:19:55 -0500

    Refactor training loop to include additional loss condition buffer for early stopping by old_approx_kl

--- demo.py
+++ demo.py
@@ -214,7 +214,7 @@ def train(args, env_module, make_env):
         ) as p:
         '''
 
-        while data.global_step < args.train.total_timesteps and data.losses.policy_loss <= 0.0 and (data.losses.value_loss == 0.00 or data.losses.value_loss >= 0.064) and (data.losses.old_approx_loss == 0.00 or data.losses.old_approx_loss >= 0.04):
+        while data.global_step < args.train.total_timesteps and data.losses.policy_loss <= 0.0 and (data.losses.value_loss == 0.00 or data.losses.value_loss >= 0.064) and (data.losses.old_approx_kl == 0.00 or data.losses.old_approx_kl >= 0.04):
             try:
                 clean_pufferl.evaluate(data)
                 clean_pufferl.train(data)
