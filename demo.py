0a39a61744c220c14d93f65899c5faeb56c4a912
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-25 13:19:31 -0500

    Change the lower limit of value losses in the trianing loop

--- demo.py
+++ demo.py
@@ -217,7 +217,7 @@ def train(args, env_module, make_env):
         ) as p:
         '''
 
-        while data.global_step < args.train.total_timesteps and data.losses.policy_loss <= 0.0 and (data.losses.value_loss == 0.00 or data.losses.value_loss >= 0.128) and (data.losses.old_approx_kl == 0.00 or data.losses.old_approx_kl >= 0.1):
+        while data.global_step < args.train.total_timesteps and data.losses.policy_loss <= 0.0 and (data.losses.value_loss == 0.00 or data.losses.value_loss >= 0.096) and (data.losses.old_approx_kl == 0.00 or data.losses.old_approx_kl >= 0.1):
             try:
                 clean_pufferl.evaluate(data)
                 clean_pufferl.train(data)
