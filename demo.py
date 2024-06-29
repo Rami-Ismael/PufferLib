b088170b035e040473747c016434755c2838de79
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-29 01:14:40 -0500

    Refactor training loop condition in demo.py to fix negative old_approx_kl check

--- demo.py
+++ demo.py
@@ -228,7 +228,7 @@ def train(args, env_module, make_env):
                 elif not ( data.losses.value_loss >= value_loss_has_to_be_greater_than or  data.losses.value_loss == 0.00):
                     print(f"The value loss need to be greater than {value_loss_has_to_be_greater_than} and not {data.losses.value_loss}")
                     return False
-                elif data.losses.old_approx_kl <=0:
+                elif data.losses.old_approx_kl <0:
                     print(f"The approx kl is {data.losses.old_approx_kl} and it should be greater than 0")
                     return False
                 return   ( data.losses.value_loss >= value_loss_has_to_be_greater_than or data.losses.value_loss == 0.00) and  ( data.losses.approx_kl <= approx_kl_has_to_be_less_than or data.losses.approx_kl == 0.00) and ( data.losses.clipfrac >= clip_frac_has_to_be_greater_than or data.losses.clipfrac == 0.00)
