8c6211eba4935a063d97c42edc484415c28a6a01
Rami-Ismael <rami.m.ismael@gmail.com>
2024-06-30 11:48:16 -0500

    Refactor compile mode in config.yaml to disable compilation during training

--- demo.py
+++ demo.py
@@ -155,15 +155,13 @@ def sweep(args, wandb_name, env_module, make_env):
             import traceback
             traceback.print_exc()
 
-    wandb.agent(sweep_id, main, count=100)
-    '''
+    #wandb.agent(sweep_id, main, count=100)
     wandb.agent(
-        sweep_id = "jaouj1pt" , 
+        sweep_id = "cr4aw4y7" , 
         project = "pufferlib" , 
         function = main,
         count = 100
     )
-    '''
     
 
 def train(args, env_module, make_env):
