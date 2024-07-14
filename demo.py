f24ad0b1df2cb6f234c615cb51595b213354dd9f
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-14 11:45:29 -0500

    Refactor code to handle exception when updating env configuration

--- demo.py
+++ demo.py
@@ -280,7 +280,7 @@ def train(args, env_module, make_env):
                 clipfrac_has_to_be_greater_than = 0.1
                 kl_has_to_be_less_than = 1
                 value_loss_has_to_be_greater_than = 0.02
-                if data.losses.value_loss < value_loss_has_to_be_greater_than:
+                if data.losses.value_loss !=0 and data.losses.value_loss < value_loss_has_to_be_greater_than:
                     print(f"The value loss is {data.losses.value_loss} and it should be greater than {value_loss_has_to_be_greater_than}")
                     return False
                 if data.losses.policy_loss > policy_loss_has_to_be_less_than :
