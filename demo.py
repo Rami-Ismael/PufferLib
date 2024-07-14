dfd04e0db6a7bbe5093c92267664d0e49dcb3552
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-14 16:09:06 -0500

    Refactor demo.py to check value loss before returning in train function

--- demo.py
+++ demo.py
@@ -278,7 +278,7 @@ def train(args, env_module, make_env):
                 policy_loss_has_to_be_less_than = 1.707766205072403e-04
                 approx_kl_has_to_be_greater_than = 0.07469981660506164
                 clipfrac_has_to_be_greater_than = 0.1
-                kl_has_to_be_less_than = 1
+                kl_has_to_be_less_than = 1.091
                 value_loss_has_to_be_greater_than = 0.02
                 if data.losses.value_loss !=0 and data.losses.value_loss < value_loss_has_to_be_greater_than:
                     print(f"The value loss is {data.losses.value_loss} and it should be greater than {value_loss_has_to_be_greater_than}")
