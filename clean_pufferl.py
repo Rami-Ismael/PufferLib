e80f15ad9f31ce58bfbbf04dfc460796ac383383
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-16 12:25:44 -0500

    Refactor clean_pufferl.py to update optimizer initialization with eps=1e-5

--- clean_pufferl.py
+++ clean_pufferl.py
@@ -56,7 +56,8 @@ def create(config, vecenv, policy, optimizer=None, wandb=None):
         policy = torch.compile(policy, mode=config.compile_mode)
     
     if config.optimizer == "AdamWScheduleFree":
-        optimizer = schedulefree.AdamWScheduleFree(policy.parameters(), lr = config.learning_rate , weight_decay = config.weight_decay , eps=1e-5)
+        optimizer = schedulefree.AdamWScheduleFree(policy.parameters(), lr = config.learning_rate , weight_decay = config.weight_decay , 
+                                                   eps = config.eps)
         optimizer.train()
     elif config.optimizer == "Adam":
         optimizer = torch.optim.Adam(policy.parameters(),
