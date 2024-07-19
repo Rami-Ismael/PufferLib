7912c57242d56b74ade79364fae97f64878f45bd
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-19 18:56:38 -0500

    Refactor config.yaml to update max_episode_steps option for puffertank_pokemon_red

--- demo.py
+++ demo.py
@@ -169,15 +169,13 @@ def sweep(args, wandb_name, env_module, make_env):
             import traceback
             traceback.print_exc()
 
-    wandb.agent(sweep_id, main, count=100)
-    '''
+    #wandb.agent(sweep_id, main, count=100)
     wandb.agent(
-        sweep_id = "usmciusr" , 
+        sweep_id = "oottl18v" , 
         project = "pufferlib" , 
         function = main,
         count = 100
     )
-    '''
     
 
 def train(args, env_module, make_env):
