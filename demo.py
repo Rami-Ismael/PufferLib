3b333e0d17c794d532685042f8dd1dafe3b7cfc0
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-18 22:52:44 -0500

    Refactor config.yaml to add clip_coef values for puffertank_pokemon_red

--- demo.py
+++ demo.py
@@ -246,7 +246,7 @@ def train(args, env_module, make_env):
                     return False
                 return   ( data.losses.value_loss >= value_loss_has_to_be_greater_than or data.losses.value_loss == 0.00) and  ( data.losses.approx_kl <= approx_kl_has_to_be_less_than or data.losses.approx_kl == 0.00) and ( data.losses.clipfrac >= clip_frac_has_to_be_greater_than or data.losses.clipfrac == 0.00)
             if train_config.update_epochs == 4:
-                policy_loss_has_to_be_less_than = .00626040246384218
+                policy_loss_has_to_be_less_than = 0
                 value_loss_has_to_be_greater_than = 0.02
                 approx_kl_has_to_be_less_than = 1.0
                 clip_frac_has_to_be_greater_than = 0.002
