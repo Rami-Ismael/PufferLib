266c7e7853d4200f154e108eff40241e43f21036
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-25 16:10:56 -0500

    Refactor config.yaml to add reward_for_using_bad_moves_coef option

--- pufferlib/environments/pokemon_red/environment.py
+++ pufferlib/environments/pokemon_red/environment.py
@@ -30,6 +30,7 @@ def make(name, headless: bool = True, state_path=None ,
         random_starter_pokemon:bool = True  , 
         negative_reward_for_wiping_out_coef = 1.0,
         negative_reward_for_entering_a_trainer_battle_lower_total_pokemon_level_coef:float = 1.0 ,
+        reward_for_using_bad_moves_coef:float =1.0
          ):
     #print(f"The current diosplayin of interval divisor is {display_info_interval_divisor}")
     '''Pokemon Red'''
@@ -43,6 +44,7 @@ def make(name, headless: bool = True, state_path=None ,
                         reward_for_entering_a_trainer_battle_coef = reward_for_entering_a_trainer_battle_coef , 
                         negative_reward_for_wiping_out_coef = negative_reward_for_wiping_out_coef,
                         negative_reward_for_entering_a_trainer_battle_lower_total_pokemon_level_coef = negative_reward_for_entering_a_trainer_battle_lower_total_pokemon_level_coef ,
+                        reward_for_using_bad_moves_coef = reward_for_using_bad_moves_coef ,
                         )
     env = RenderWrapper(env)
     if EpisodeStats:
