d75a753055dfaa6b0f9ff9eef47acfa54f6719b2
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-27 17:24:19 -0500

    Chnage the hyperparameter value range for clip coeficients

--- pufferlib/environments/pokemon_red/environment.py
+++ pufferlib/environments/pokemon_red/environment.py
@@ -31,7 +31,7 @@ def make(name, headless: bool = True, state_path=None ,
         negative_reward_for_wiping_out_coef = 1.0,
         negative_reward_for_entering_a_trainer_battle_lower_total_pokemon_level_coef:float = 1.0 ,
         reward_for_using_bad_moves_coef:float =1.0 ,
-        disable_wild_encounters:bool = False ,
+        disable_wild_encounters:bool =  True,
          ):
     #print(f"The current diosplayin of interval divisor is {display_info_interval_divisor}")
     '''Pokemon Red'''
