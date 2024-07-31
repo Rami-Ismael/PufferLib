e1fe1b3b43e6d207886b9e8e1ede677784802dd9
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-31 17:58:51 -0500

    refactor: Remove commented code and update wandb agent in demo.py

--- demo.py
+++ demo.py
@@ -169,13 +169,13 @@ def sweep(args, wandb_name, env_module, make_env):
             import traceback
             traceback.print_exc()
 
-    #wandb.agent(sweep_id, main, count=300)
-    wandb.agent(
-        sweep_id = "rxwnqt5i" , 
-        project = "pufferlib" , 
-        function = main,
-        count = 300
-    )
+    wandb.agent(sweep_id, main, count=300)
+    #wandb.agent(
+    #    sweep_id = "rxwnqt5i" , 
+    #    project = "pufferlib" , 
+    #    function = main,
+    #    count = 300
+    #)
     
 
 def train(args, env_module, make_env):
