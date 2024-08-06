4ac82036acbbc79b4b24d3114342e7d5b200215c
Rami-Ismael <rami.m.ismael@gmail.com>
2024-08-05 23:13:22 -0500

    refactor: Update train configuration and wandb agent settings

--- demo.py
+++ demo.py
@@ -270,7 +270,7 @@ def train(args, env_module, make_env):
                 return   ( data.losses.value_loss >= value_loss_has_to_be_greater_than or data.losses.value_loss == 0.00) and  ( data.losses.approx_kl <= approx_kl_has_to_be_less_than or data.losses.approx_kl == 0.00) and ( data.losses.clipfrac >= clip_frac_has_to_be_greater_than or data.losses.clipfrac == 0.00)
             if train_config.update_epochs == 8:
                 policy_loss_has_to_be_less_than = 0.0015938752330839634
-                approx_kl_has_to_be_greater_than = 0.009055721864569932
+                approx_kl_has_to_be_greater_than = 0.008041739416512428
                 clipfrac_has_to_be_greater_than = 0.004302978515625
                 if data.losses.policy_loss > policy_loss_has_to_be_less_than :
                     print(f"The policy loss is {data.losses.policy_loss}")
