2490c412b5fef2ac0a2f27d504a76960da4d66fd
Rami-Ismael <rami.m.ismael@gmail.com>
2024-08-09 01:20:48 -0500

    refactor: Adjust approx_kl threshold in demo.py

--- demo.py
+++ demo.py
@@ -251,7 +251,7 @@ def train(args, env_module, make_env):
                 value_loss_has_to_be_greater_than = 0.0045
                 approx_kl_has_to_be_less_than = 1.0
                 clip_frac_has_to_be_greater_than = 0.002
-                approx_kl_has_to_be_greater_than = 0.0037
+                approx_kl_has_to_be_greater_than = 0.0033
                 if data.losses.policy_loss > policy_loss_has_to_be_less_than :
                     print(f"The policy loss is {data.losses.policy_loss}")
                     return False
