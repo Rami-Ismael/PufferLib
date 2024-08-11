c2a651766bb6b6dc73cd8cc4da4211098367ad81
Rami-Ismael <rami.m.ismael@gmail.com>
2024-08-10 23:16:21 -0500

    refactor: Adjust approx_kl threshold in demo.py for update epcih 4

--- demo.py
+++ demo.py
@@ -252,7 +252,7 @@ def train(args, env_module, make_env):
                 value_loss_has_to_be_greater_than = 0.0024
                 approx_kl_has_to_be_less_than = 1.0
                 clip_frac_has_to_be_greater_than = 0.002
-                approx_kl_has_to_be_greater_than = 0.0027
+                approx_kl_has_to_be_greater_than = 0.001674623420854914
                 if data.losses.policy_loss > policy_loss_has_to_be_less_than :
                     print(f"The policy loss is {data.losses.policy_loss}")
                     return False
