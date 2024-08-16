4124986a984f0bd44c46420255734b3ad1b34a98
Rami-Ismael <rami.m.ismael@gmail.com>
2024-08-16 02:50:18 -0500

    refactor: Adjust value loss threshold and approx_kl_has_to_be_greater_than conditions in demo.py for update epochs 2 and 4

--- demo.py
+++ demo.py
@@ -230,7 +230,7 @@ def train(args, env_module, make_env):
         def early_stopping_base_on_ppo_loss_func(data):
             if train_config.update_epochs == 2:
                 policy_loss_has_to_be_less_than =  0.003
-                value_loss_has_to_be_greater_than = 0.0005441446857934352
+                value_loss_has_to_be_greater_than = 0.00025
                 approx_kl_has_to_be_less_than = 1.0
                 clip_frac_has_to_be_greater_than = 0.002
                 explain_variance_has_to_be_greater_than = 0.0
@@ -251,10 +251,10 @@ def train(args, env_module, make_env):
                     return False
             if train_config.update_epochs == 4:
                 policy_loss_has_to_be_less_than = 0.019811158068478107
-                value_loss_has_to_be_greater_than = 0.0005441446857934352
+                value_loss_has_to_be_greater_than = 0.00025
                 approx_kl_has_to_be_less_than = 1.0
                 clip_frac_has_to_be_greater_than = 0.002
-                approx_kl_has_to_be_greater_than = 0.00075
+                approx_kl_has_to_be_greater_than = 0.00058
                 if data.losses.policy_loss > policy_loss_has_to_be_less_than :
                     print(f"The policy loss is {data.losses.policy_loss}")
                     return False
